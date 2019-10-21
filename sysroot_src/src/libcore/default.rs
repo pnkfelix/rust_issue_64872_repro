ff88a08ff50c9b7c729dc74867f2e0a12e3ad17b
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-10-21 17:04:38 +0200

    libcore minification

--- sysroot_src/src/libcore/default.rs
+++ sysroot_src/src/libcore/default.rs
@@ -2,120 +2,12 @@
 
 #![stable(feature = "rust1", since = "1.0.0")]
 
-/// A trait for giving a type a useful default value.
-///
-/// Sometimes, you want to fall back to some kind of default value, and
-/// don't particularly care what it is. This comes up often with `struct`s
-/// that define a set of options:
-///
-/// ```
-/// # #[allow(dead_code)]
-/// struct SomeOptions {
-///     foo: i32,
-///     bar: f32,
-/// }
-/// ```
-///
-/// How can we define some default values? You can use `Default`:
-///
-/// ```
-/// # #[allow(dead_code)]
-/// #[derive(Default)]
-/// struct SomeOptions {
-///     foo: i32,
-///     bar: f32,
-/// }
-///
-/// fn main() {
-///     let options: SomeOptions = Default::default();
-/// }
-/// ```
-///
-/// Now, you get all of the default values. Rust implements `Default` for various primitives types.
-///
-/// If you want to override a particular option, but still retain the other defaults:
-///
-/// ```
-/// # #[allow(dead_code)]
-/// # #[derive(Default)]
-/// # struct SomeOptions {
-/// #     foo: i32,
-/// #     bar: f32,
-/// # }
-/// fn main() {
-///     let options = SomeOptions { foo: 42, ..Default::default() };
-/// }
-/// ```
-///
-/// ## Derivable
-///
-/// This trait can be used with `#[derive]` if all of the type's fields implement
-/// `Default`. When `derive`d, it will use the default value for each field's type.
-///
-/// ## How can I implement `Default`?
-///
-/// Provides an implementation for the `default()` method that returns the value of
-/// your type that should be the default:
-///
-/// ```
-/// # #![allow(dead_code)]
-/// enum Kind {
-///     A,
-///     B,
-///     C,
-/// }
-///
-/// impl Default for Kind {
-///     fn default() -> Self { Kind::A }
-/// }
-/// ```
-///
-/// # Examples
-///
-/// ```
-/// # #[allow(dead_code)]
-/// #[derive(Default)]
-/// struct SomeOptions {
-///     foo: i32,
-///     bar: f32,
-/// }
-/// ```
 #[stable(feature = "rust1", since = "1.0.0")]
 pub trait Default: Sized {
-    /// Returns the "default value" for a type.
-    ///
-    /// Default values are often some kind of initial value, identity value, or anything else that
-    /// may make sense as a default.
-    ///
-    /// # Examples
-    ///
-    /// Using built-in default values:
-    ///
-    /// ```
-    /// let i: i8 = Default::default();
-    /// let (x, y): (Option<String>, f64) = Default::default();
-    /// let (a, b, (c, d)): (i32, u32, (bool, bool)) = Default::default();
-    /// ```
-    ///
-    /// Making your own:
-    ///
-    /// ```
-    /// # #[allow(dead_code)]
-    /// enum Kind {
-    ///     A,
-    ///     B,
-    ///     C,
-    /// }
-    ///
-    /// impl Default for Kind {
-    ///     fn default() -> Self { Kind::A }
-    /// }
-    /// ```
     #[stable(feature = "rust1", since = "1.0.0")]
     fn default() -> Self;
 }
 
-/// Derive macro generating an impl of the trait `Default`.
 #[rustc_builtin_macro]
 #[stable(feature = "builtin_macro_prelude", since = "1.38.0")]
 #[allow_internal_unstable(core_intrinsics)]
@@ -127,7 +19,7 @@ macro_rules! default_impl {
         impl Default for $t {
             #[inline]
             #[doc = $doc]
-            fn default() -> $t { $v }
+            fn default() -> $t { loop { } }
         }
     }
 }
--- sysroot_src/src/libcore/panic.rs
+++ sysroot_src/src/libcore/panic.rs
@@ -7,28 +7,6 @@
 use crate::any::Any;
 use crate::fmt;
 
-/// A struct providing information about a panic.
-///
-/// `PanicInfo` structure is passed to a panic hook set by the [`set_hook`]
-/// function.
-///
-/// [`set_hook`]: ../../std/panic/fn.set_hook.html
-///
-/// # Examples
-///
-/// ```should_panic
-/// use std::panic;
-///
-/// panic::set_hook(Box::new(|panic_info| {
-///     if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
-///         println!("panic occurred: {:?}", s);
-///     } else {
-///         println!("panic occurred");
-///     }
-/// }));
-///
-/// panic!("Normal panic");
-/// ```
 #[lang = "panic_info"]
 #[stable(feature = "panic_hooks", since = "1.10.0")]
 #[derive(Debug)]
@@ -47,121 +25,27 @@ impl<'a> PanicInfo<'a> {
     #[inline]
     pub fn internal_constructor(message: Option<&'a fmt::Arguments<'a>>,
                                 location: Location<'a>)
-                                -> Self {
-        struct NoPayload;
-        PanicInfo { payload: &NoPayload, location, message }
-    }
+                                -> Self { loop { } }
 
     #[doc(hidden)]
     #[inline]
-    pub fn set_payload(&mut self, info: &'a (dyn Any + Send)) {
-        self.payload = info;
-    }
+    pub fn set_payload(&mut self, info: &'a (dyn Any + Send)) { loop { } }
 
-    /// Returns the payload associated with the panic.
-    ///
-    /// This will commonly, but not always, be a `&'static str` or [`String`].
-    ///
-    /// [`String`]: ../../std/string/struct.String.html
-    ///
-    /// # Examples
-    ///
-    /// ```should_panic
-    /// use std::panic;
-    ///
-    /// panic::set_hook(Box::new(|panic_info| {
-    ///     println!("panic occurred: {:?}", panic_info.payload().downcast_ref::<&str>().unwrap());
-    /// }));
-    ///
-    /// panic!("Normal panic");
-    /// ```
     #[stable(feature = "panic_hooks", since = "1.10.0")]
-    pub fn payload(&self) -> &(dyn Any + Send) {
-        self.payload
-    }
+    pub fn payload(&self) -> &(dyn Any + Send) { loop { } }
 
-    /// If the `panic!` macro from the `core` crate (not from `std`)
-    /// was used with a formatting string and some additional arguments,
-    /// returns that message ready to be used for example with [`fmt::write`]
-    ///
-    /// [`fmt::write`]: ../fmt/fn.write.html
     #[unstable(feature = "panic_info_message", issue = "44489")]
-    pub fn message(&self) -> Option<&fmt::Arguments<'_>> {
-        self.message
-    }
+    pub fn message(&self) -> Option<&fmt::Arguments<'_>> { loop { } }
 
-    /// Returns information about the location from which the panic originated,
-    /// if available.
-    ///
-    /// This method will currently always return [`Some`], but this may change
-    /// in future versions.
-    ///
-    /// [`Some`]: ../../std/option/enum.Option.html#variant.Some
-    ///
-    /// # Examples
-    ///
-    /// ```should_panic
-    /// use std::panic;
-    ///
-    /// panic::set_hook(Box::new(|panic_info| {
-    ///     if let Some(location) = panic_info.location() {
-    ///         println!("panic occurred in file '{}' at line {}", location.file(),
-    ///             location.line());
-    ///     } else {
-    ///         println!("panic occurred but can't get location information...");
-    ///     }
-    /// }));
-    ///
-    /// panic!("Normal panic");
-    /// ```
     #[stable(feature = "panic_hooks", since = "1.10.0")]
-    pub fn location(&self) -> Option<&Location<'_>> {
-        // NOTE: If this is changed to sometimes return None,
-        // deal with that case in std::panicking::default_hook and std::panicking::begin_panic_fmt.
-        Some(&self.location)
-    }
+    pub fn location(&self) -> Option<&Location<'_>> { loop { } }
 }
 
 #[stable(feature = "panic_hook_display", since = "1.26.0")]
 impl fmt::Display for PanicInfo<'_> {
-    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        formatter.write_str("panicked at ")?;
-        if let Some(message) = self.message {
-            write!(formatter, "'{}', ", message)?
-        } else if let Some(payload) = self.payload.downcast_ref::<&'static str>() {
-            write!(formatter, "'{}', ", payload)?
-        }
-        // NOTE: we cannot use downcast_ref::<String>() here
-        // since String is not available in libcore!
-        // The payload is a String when `std::panic!` is called with multiple arguments,
-        // but in that case the message is also available.
-
-        self.location.fmt(formatter)
-    }
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result { loop { } }
 }
 
-/// A struct containing information about the location of a panic.
-///
-/// This structure is created by the [`location`] method of [`PanicInfo`].
-///
-/// [`location`]: ../../std/panic/struct.PanicInfo.html#method.location
-/// [`PanicInfo`]: ../../std/panic/struct.PanicInfo.html
-///
-/// # Examples
-///
-/// ```should_panic
-/// use std::panic;
-///
-/// panic::set_hook(Box::new(|panic_info| {
-///     if let Some(location) = panic_info.location() {
-///         println!("panic occurred in file '{}' at line {}", location.file(), location.line());
-///     } else {
-///         println!("panic occurred but can't get location information...");
-///     }
-/// }));
-///
-/// panic!("Normal panic");
-/// ```
 #[derive(Debug)]
 #[stable(feature = "panic_hooks", since = "1.10.0")]
 pub struct Location<'a> {
@@ -176,87 +60,23 @@ impl<'a> Location<'a> {
                           and related macros",
                 issue = "0")]
     #[doc(hidden)]
-    pub fn internal_constructor(file: &'a str, line: u32, col: u32) -> Self {
-        Location { file, line, col }
-    }
+    pub fn internal_constructor(file: &'a str, line: u32, col: u32) -> Self { loop { } }
 
-    /// Returns the name of the source file from which the panic originated.
-    ///
-    /// # Examples
-    ///
-    /// ```should_panic
-    /// use std::panic;
-    ///
-    /// panic::set_hook(Box::new(|panic_info| {
-    ///     if let Some(location) = panic_info.location() {
-    ///         println!("panic occurred in file '{}'", location.file());
-    ///     } else {
-    ///         println!("panic occurred but can't get location information...");
-    ///     }
-    /// }));
-    ///
-    /// panic!("Normal panic");
-    /// ```
     #[stable(feature = "panic_hooks", since = "1.10.0")]
-    pub fn file(&self) -> &str {
-        self.file
-    }
+    pub fn file(&self) -> &str { loop { } }
 
-    /// Returns the line number from which the panic originated.
-    ///
-    /// # Examples
-    ///
-    /// ```should_panic
-    /// use std::panic;
-    ///
-    /// panic::set_hook(Box::new(|panic_info| {
-    ///     if let Some(location) = panic_info.location() {
-    ///         println!("panic occurred at line {}", location.line());
-    ///     } else {
-    ///         println!("panic occurred but can't get location information...");
-    ///     }
-    /// }));
-    ///
-    /// panic!("Normal panic");
-    /// ```
     #[stable(feature = "panic_hooks", since = "1.10.0")]
-    pub fn line(&self) -> u32 {
-        self.line
-    }
+    pub fn line(&self) -> u32 { loop { } }
 
-    /// Returns the column from which the panic originated.
-    ///
-    /// # Examples
-    ///
-    /// ```should_panic
-    /// use std::panic;
-    ///
-    /// panic::set_hook(Box::new(|panic_info| {
-    ///     if let Some(location) = panic_info.location() {
-    ///         println!("panic occurred at column {}", location.column());
-    ///     } else {
-    ///         println!("panic occurred but can't get location information...");
-    ///     }
-    /// }));
-    ///
-    /// panic!("Normal panic");
-    /// ```
     #[stable(feature = "panic_col", since = "1.25.0")]
-    pub fn column(&self) -> u32 {
-        self.col
-    }
+    pub fn column(&self) -> u32 { loop { } }
 }
 
 #[stable(feature = "panic_hook_display", since = "1.26.0")]
 impl fmt::Display for Location<'_> {
-    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(formatter, "{}:{}:{}", self.file, self.line, self.col)
-    }
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result { loop { } }
 }
 
-/// An internal trait used by libstd to pass data from libstd to `panic_unwind`
-/// and other panic runtimes. Not intended to be stabilized any time soon, do
-/// not use.
 #[unstable(feature = "std_internals", issue = "0")]
 #[doc(hidden)]
 pub unsafe trait BoxMeUp {
