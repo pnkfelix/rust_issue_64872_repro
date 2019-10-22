f8d8eceaefaa2129a9f000db050f03d2118cc384
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-10-22 21:43:33 +0200

    minifcation of compiler_builtins crate for this bug.

--- sysroot_src/src/compiler_builtins/src/lib.rs
+++ sysroot_src/src/compiler_builtins/src/lib.rs
@@ -1,35 +1,10 @@
-#![cfg_attr(not(stage0), deny(warnings))]
-#![cfg_attr(not(test), no_std)]
-#![cfg_attr(feature = "compiler-builtins", compiler_builtins)]
+#![no_std]
+#![compiler_builtins]
 #![crate_name = "compiler_builtins"]
 #![crate_type = "rlib"]
-#![doc(
-    html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
-    html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
-    html_root_url = "https://doc.rust-lang.org/nightly/",
-    html_playground_url = "https://play.rust-lang.org/",
-    test(attr(deny(warnings)))
-)]
 #![feature(compiler_builtins)]
 #![feature(naked_functions)]
-#![feature(repr_simd)]
-#![feature(abi_unadjusted)]
-#![feature(linkage)]
-#![feature(lang_items)]
-#![allow(unused_features)]
-#![no_builtins]
-#![cfg_attr(feature = "compiler-builtins", feature(staged_api))]
-#![cfg_attr(
-    feature = "compiler-builtins",
-    unstable(
-        feature = "compiler_builtins_lib",
-        reason = "Compiler builtins. Will never become stable.",
-        issue = "0"
-    )
-)]
 pub mod probestack {
-    #![cfg(not(windows))] // Windows already has builtins to do this
-
     #[naked]
     #[no_mangle]
     pub unsafe extern "C" fn __rust_probestack() { }
