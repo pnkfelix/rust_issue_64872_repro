acec92430e6072c862c3fb6265cb4b63569daadf
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-10-22 16:52:59 +0200

    further minification.

--- sysroot_src/src/libcore/ops/mod.rs
+++ sysroot_src/src/libcore/ops/mod.rs
@@ -3,7 +3,6 @@
 #![stable(feature = "rust1", since = "1.0.0")]
 
 mod deref;
-mod drop;
 mod function;
 mod unsize;
 
@@ -12,9 +11,6 @@ pub use self::deref::{Deref, DerefMut};
 
 #[unstable(feature = "receiver_trait", issue = "0")]
 pub use self::deref::Receiver;
-
-#[stable(feature = "rust1", since = "1.0.0")]
-pub use self::drop::Drop;
 
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use self::function::{Fn, FnMut, FnOnce};
