e76bdcf2a5d3eb3b9e0032b9a0b525e95eb68858
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-10-22 15:02:50 +0200

    further minification.

--- sysroot_src/src/libcore/lib.rs
+++ sysroot_src/src/libcore/lib.rs
@@ -143,9 +143,6 @@ pub mod marker {
 
     #[lang = "freeze"]
     pub(crate) unsafe auto trait Freeze {}
-    unsafe impl<T: ?Sized> Freeze for PhantomData<T> {}
-    unsafe impl<T: ?Sized> Freeze for *const T {}
-    unsafe impl<T: ?Sized> Freeze for *mut T {}
     unsafe impl<T: ?Sized> Freeze for &T {}
     unsafe impl<T: ?Sized> Freeze for &mut T {}
 
@@ -169,39 +166,6 @@ pub mod marker {
     #[unstable(feature = "unsize", issue = "27732")]
     #[lang = "unsize"]
     pub trait Unsize<T: ?Sized> { }
-
-
-    mod copy_impls {
-        use super::Copy;
-
-        macro_rules! impl_copy {
-            ($($t:ty)*) => {
-                $(
-                    #[stable(feature = "rust1", since = "1.0.0")]
-                    impl Copy for $t {}
-                )*
-            }
-        }
-
-        impl_copy! {
-            usize u8 u16 u32 u64 u128
-                isize i8 i16 i32 i64 i128
-                f32 f64
-                bool char
-        }
-
-        #[unstable(feature = "never_type", issue = "35121")]
-        impl Copy for ! {}
-
-        #[stable(feature = "rust1", since = "1.0.0")]
-        impl<T: ?Sized> Copy for *const T {}
-
-        #[stable(feature = "rust1", since = "1.0.0")]
-        impl<T: ?Sized> Copy for *mut T {}
-
-        #[stable(feature = "rust1", since = "1.0.0")]
-        impl<T: ?Sized> Copy for &T {}
-    }
 }
 
 
