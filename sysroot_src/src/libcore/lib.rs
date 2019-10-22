0947dbc897abda6dc5667bdb4decfdaf987507f3
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-10-22 11:51:59 +0200

    further minimization.

--- sysroot_src/src/libcore/lib.rs
+++ sysroot_src/src/libcore/lib.rs
@@ -89,30 +89,22 @@ mod macros;
 #[macro_use]
 mod internal_macros;
 
-#[path = "num/int_macros.rs"]
-#[macro_use]
-mod int_macros;
+#[stable(feature = "rust1", since = "1.0.0")] pub mod isize { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod i8 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod i16 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod i32 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod i64 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod i128 { }
 
-#[path = "num/uint_macros.rs"]
-#[macro_use]
-mod uint_macros;
+#[stable(feature = "rust1", since = "1.0.0")] pub mod usize { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod u8 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod u16 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod u32 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod u64 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod u128 { }
 
-#[path = "num/isize.rs"] pub mod isize;
-#[path = "num/i8.rs"]    pub mod i8;
-#[path = "num/i16.rs"]   pub mod i16;
-#[path = "num/i32.rs"]   pub mod i32;
-#[path = "num/i64.rs"]   pub mod i64;
-#[path = "num/i128.rs"]  pub mod i128;
-
-#[path = "num/usize.rs"] pub mod usize;
-#[path = "num/u8.rs"]    pub mod u8;
-#[path = "num/u16.rs"]   pub mod u16;
-#[path = "num/u32.rs"]   pub mod u32;
-#[path = "num/u64.rs"]   pub mod u64;
-#[path = "num/u128.rs"]  pub mod u128;
-
-#[path = "num/f32.rs"]   pub mod f32;
-#[path = "num/f64.rs"]   pub mod f64;
+#[stable(feature = "rust1", since = "1.0.0")] pub mod f32 { }
+#[stable(feature = "rust1", since = "1.0.0")] pub mod f64 { }
 
 #[macro_use]
 pub mod num;
